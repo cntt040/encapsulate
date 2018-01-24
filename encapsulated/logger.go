518fd2c5c7adb331763bb6d11e2c925bfa21491a
TamNT <ghn.tech.tamnt@gmail.com>
2018-01-24 10:35:19 +0700

    save point

--- encapsulated/logger.go
+++ encapsulated/logger.go
@@ -32,7 +32,7 @@ func GetLogger(module string) *logging.Logger {
 func GetLoggerHTTP(module string) *logging.Logger {
 	log = logging.MustGetLogger(module)
 	var format = logging.MustStringFormatter(
-		`%{color}%{time:15:04:05.000}%{color:reset} %{message}`,
+		`{"time":%{time:15:04:05.000},"message":%{message}}`,
 	)
 	logBackend := logging.NewLogBackend(os.Stdout, "*", 0)
 	logBackendFormatter := logging.NewBackendFormatter(logBackend, format)
